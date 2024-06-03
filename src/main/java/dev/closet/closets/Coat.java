fc20e6743d4b8cd85bed869a2382b00752b01cd3
Alin1911 <andreialin2324@yahoo.com>
2024-06-03 19:30:10 +0300

    add models

---
+++ src/main/java/dev/closet/closets/Coat.java
@@ -0,0 +1,18 @@
+package dev.closet.closets;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collation = "clothes")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Coat {
+    @Id
+    public ObjectId id;
+    private String body;
+}
