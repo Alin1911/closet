099fc0401abce0b1752428113a7a19d2edf4aa4d
Alin1911 <andreialin2324@yahoo.com>
2024-06-06 23:47:47 +0300

    fix

--- src/main/java/dev/closet/closets/ClosetRepository.java
+++ src/main/java/dev/closet/closets/ClosetRepository.java
@@ -8,5 +8,7 @@ import org.springframework.stereotype.Repository;
 public interface ClosetRepository extends MongoRepository<Closet, ObjectId> {
     /*
         This interface extends MongoRepository which is a part of Spring Data MongoDB.
+
+        The MongoRepository interface provides methods for performing CRUD operations on a MongoDB database.
     */
 }
