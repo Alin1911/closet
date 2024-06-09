e74d6a55927c4ed3c53465a0692127a846c814e8
Alin1911 <andreialin2324@yahoo.com>
2024-06-09 18:44:01 +0300

    react configur

---
+++ closet-client-v1/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import api from './api/axiosConfig';
+import { useState, useEffect } from 'react';
+
+function App() {
+  const [closets, setClosets] = useState([]);
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
