6e0e4c3f6e5d5b16c09426632cb65427507afe0d
Alin1911 <andreialin2324@yahoo.com>
2024-06-09 19:23:24 +0300

    cross origin and first axios request

--- closet-client-v1/src/App.js
+++ closet-client-v1/src/App.js
@@ -5,6 +5,19 @@ import { useState, useEffect } from 'react';
 function App() {
   const [closets, setClosets] = useState([]);
 
+  const getClosets = async () => {
+    try {
+      const response = await api.get('/api/v1/closets');
+      console.log(response.data);
+      setClosets(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  useEffect(() => {
+    getClosets();
+  }, []);
+
   return (
     <div className="App">
 
--- closet-client-v1/src/api/axiosConfig.js
+++ closet-client-v1/src/api/axiosConfig.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 
 export default axios.create({
-    baseURL: 'http://localhost:8080/api/v1/',
-    headers: {"ngrok-skip-browser-warning": "true"}
+    baseURL: 'http://localhost:8080',
+    headers: {
+        "ngrok-skip-browser-warning": "true"
+    }
 });
