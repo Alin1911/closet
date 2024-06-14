ca21f6cc450fb4040b0c0b333641d6c63fb284a3
Alin1911 <andreialin2324@yahoo.com>
2024-06-14 20:19:59 +0300

    style carousel poster

--- closet-client-v1/src/components/hero/Hero.js
+++ closet-client-v1/src/components/hero/Hero.js
@@ -12,12 +12,14 @@ import { Paper } from '@mui/material'
                      return (
                         <Paper>
                             <div className='closet-carousel-container'>
-                                <div className='closet-card'>
-                                    <div className='closet-poster'>
-                                        <img src={closet.poster} alt="" />
-                                    </div>
-                                    <div className='closet-title'>
-                                        <h3>{closet.title}</h3>
+                                <div className='closet-card' style={{"--img": `url(${closet.backdrops[0]})`}}>
+                                    <div className='closet-detail'>
+                                        <div className='closet-poster'>
+                                            <img src={closet.poster} alt="" />
+                                        </div>
+                                        <div className='closet-title'>
+                                            <h3>{closet.title}</h3>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
